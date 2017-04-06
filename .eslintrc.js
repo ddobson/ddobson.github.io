716114c1c6c0107687d70d3b1534342192ebe307
ddobson <ddobson445@gmail.com>
2017-04-06 11:06:52 -0400

    Remove react specific concerns from eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,68 +5,43 @@ module.exports = {
         "es6": true
     },
     "extends": "eslint:recommended",
-    "parserOptions": {
-        "ecmaFeatures": {
-            "experimentalObjectRestSpread": true,
-            "jsx": true
-        },
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react"
-    ],
     "rules": {
-        "comma-spacing": 2,
-        "key-spacing": [
-          "error", {
-            "align": {
-              "beforeColon": false,
-              "afterColon": false,
-              "on": "value"
-            }
+      "comma-spacing": 2,
+      "key-spacing": [
+        "error", {
+          "align": {
+            "beforeColon": false,
+            "afterColon": false,
+            "on": "value"
           }
-        ],
-        "no-console": 1,
-        "prefer-const": 2,
-        "prefer-arrow-callback": 1,
-        "indent": [
-            "error",
-            2
-        ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "quotes": [
-            "error",
-            "single"
-        ],
-        "semi": [
-            "error",
-            "never"
-        ],
-        "no-var": 2,
-        "dot-notation": [
-            "warn"
-        ],
-        "object-curly-spacing": [
-            "warn",
-            "always"
-        ],
-        "no-useless-constructor": 2,
-        "jsx-quotes": ["error", "prefer-double"],
-        "react/prefer-stateless-function": 2,
-        "react/prefer-es6-class": [2, "always"],
-        "react/jsx-no-undef": 2,
-        "react/jsx-uses-vars": 2,
-        "react/jsx-key": 2,
-        "react/jsx-no-bind": [
-            2,
-            {
-              "ignoreRefs": true,
-              "allowArrowFunctions": true,
-              "allowBind": false
-            }
-        ]
+        }
+      ],
+      "no-console": 1,
+      "prefer-const": 2,
+      "prefer-arrow-callback": 1,
+      "indent": [
+          "error",
+          2
+      ],
+      "linebreak-style": [
+          "error",
+          "unix"
+      ],
+      "quotes": [
+          "error",
+          "single"
+      ],
+      "semi": [
+          "error",
+          "never"
+      ],
+      "no-var": 2,
+      "dot-notation": [
+          "warn"
+      ],
+      "object-curly-spacing": [
+          "warn",
+          "always"
+      ]
     }
 };
